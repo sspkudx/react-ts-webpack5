cb9bb3caa07e9fc564cf48ea0b594205189f7926
yueb <zhaozhengyang_byn@pku.edu.cn>
2024-02-09 10:36:03 +0800

    remove redundant comments

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
     env: {
         browser: true,
-        es2022: true,
+        es2023: true,
         node: true,
     },
     extends: [
@@ -55,54 +55,6 @@ module.exports = {
                 allowBind: true,
             },
         ],
-        /**
-         * @see
-         * - 'function-declaration' is used in the following manner:
-         *
-         * ```
-         * function ReactComponent(props: ReactComponent) {
-         *     ...
-         * }
-         * ```
-         * - 'arrow-function' is utilised as shown below:
-         *
-         * ```
-         * const ReactComponent: React.FC<ReactComponent> = props => {
-         *     ...
-         * };
-         * ```
-         *
-         * Both styles are valid. You can modify this rule based on your specific needs.
-         * For instance, if 'arrow-function' is predominantly used in your team, you can
-         * adjust the rule as follows:
-         * 'react/function-component-definition': [2, {
-         *      namedComponents: ['arrow-function'],
-         *      unnamedComponents: ['arrow-function'],
-         * }]
-         *
-         * @ChineseVersion
-         * - 规则'function-declaration'(用函数声明式定义组件)：
-         *
-         * ```
-         * function ReactComponent(props: ReactComponent) {
-         *     ...
-         * }
-         * ```
-         * - 规则'arrow-function'(用箭头函数定义组件)：
-         *
-         * ```
-         * const ReactComponent: React.FC<ReactComponent> = props => {
-         *     ...
-         * };
-         * ```
-         *
-         * 这两种方式都是可以接受的，可以根据你的需求来修改下面的规则。
-         * 例如，如果团队用箭头函数定义组件比较多，你可以将规则重写为：
-         * 'react/function-component-definition': [2, {
-         *      namedComponents: 'arrow-function',
-         *      unnamedComponents: 'arrow-function',
-         * }]
-         */
         'react/function-component-definition': [
             2,
             {
